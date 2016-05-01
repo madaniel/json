13ddeef45908f7c3c400ae2bc4ff10e07143a7ae
Daniel Madar <m.a.daniel@gmail.com>
2016-05-01 22:32:29 +0300

    Added 2 TCs for testing json_parser.py

--- test_json_parser.py
+++ test_json_parser.py
@@ -10,6 +10,13 @@ json4_values = ["on", "Sample Konfabulator Widget", "Images/Sun.png", "Click Her
 json5_keys = ["fcodeNameXXX", "lngXXX", "toponymName", "population"]
 json5_values = ["capital of a political entity", 114.157691001892, "Mexico City", 12294193]
 
+json6_keys = ["time", "milliseconds_since_epoch", "date"]
+json6_count = [2, 2, 2]
+
+json3_keys = ["long_name", "short_name", "lat", "lng"]
+json3_count = [8, 8, 3, 3]
+
+
 
 def test_get_value_tc1():
     # Testing get_value with a simple JSON
@@ -37,7 +44,20 @@ def test_get_value_tc3():
     assert json_parser.get_value(json, json5_keys[3]) == json5_values[3]
 
 
+def test_count_keys_tc1():
+    # Testing count_keys with a simple JSON
+    json = json_data.json6
+    assert json_parser.count_keys(json, json6_keys[0]) == json6_count[0]
+    assert json_parser.count_keys(json, json6_keys[1]) == json6_count[1]
+    assert json_parser.count_keys(json, json6_keys[2]) == json6_count[2]
 
+
+def test_count_keys_tc2():
+    # Testing count_keys with a complex JSON
+    json = json_data.json3
+    assert json_parser.count_keys(json, json3_keys[0]) == json3_count[0]
+    assert json_parser.count_keys(json, json3_keys[1]) == json3_count[1]
+    assert json_parser.count_keys(json, json3_keys[2]) == json3_count[2]
 
 
 
